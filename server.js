895c60ac32f89728b85f2e55b7aa4574e39ac5af
br0r <viktor@silfverstrom.com>
2015-10-05 15:18:16 +0200

    formatted server

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.docs.config');
 
 new WebpackDevServer(webpack(config), {
-  publicPath: config.output.publicPath,
-  hot: true,
-  historyApiFallback: true
-	}).listen(8090,  function (err, result) {
-  if (err) {
-    console.log(err);
-  }
+    publicPath: config.output.publicPath,
+    hot: true,
+    historyApiFallback: true
+  }).listen(8090,  function (err, result) {
+    if (err) {
+      console.log(err);
+    }
 
-  console.log('Listening on 8090');
+    console.log('Listening on 8090');
 });
