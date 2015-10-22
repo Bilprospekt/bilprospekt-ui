a36a4651f8d2d835e97d2f9b6dfc8e075136d9df
Dan Andersson <dan@bilprospekt.se>
2015-10-22 13:46:56 +0200

    Added sass/library/ folder with some important variables and other basics. Fixed styling for tooltip.

--- src/tooltip/index.js
+++ src/tooltip/index.js
@@ -102,7 +102,7 @@ const TooltipComponent = React.createClass({
     },
     render() {
         var textStyle = {
-            display : this.state.hover ? 'block' : 'none',
+            display : this.state.hover ? 'block' : 'block',
             left: this.state.left,
             top: this.state.top,
         };
