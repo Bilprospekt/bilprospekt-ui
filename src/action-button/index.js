2c30ba7c140bf36c78ccc55d2e2ba77b5b3d272e
br0r <viktor@silfverstrom.com>
2015-10-27 11:51:48 +0100

    updated button styling

--- src/action-button/index.js
+++ src/action-button/index.js
@@ -1,34 +1,45 @@
 import React, { Component } from 'react';
 import classNames           from 'classnames';
 
-var ActionButton = React.createClass({
+const ActionButton = React.createClass({
     propTypes: {
-        label:   React.PropTypes.string.isRequired,
+        label: React.PropTypes.string.isRequired,
         primary: React.PropTypes.bool,
-        minor:   React.PropTypes.bool, 
+        minor: React.PropTypes.bool,
+        style: React.PropTypes.object,
+        selected: React.PropTypes.bool,
+        flat: React.PropTypes.bool,
     },
     getDefaultProps() {
         return {
             primary: false,
             minor:   false,
+            selected: false,
+            flat: false,
         };
     },
     render() {
-        let buttonClass;
+        let primaryOrSecondaryClass;
         if (!this.props.minor) {
-            buttonClass = classNames({
+            primaryOrSecondaryClass = {
                 'bui-primary-action-button'   : this.props.primary,
                 'bui-secondary-action-button' : !this.props.primary,
-            });
+            };
         } else {
-            buttonClass = classNames({
+            primaryOrSecondaryClass = {
                 'bui-minor-primary-action-button'   : this.props.primary,
                 'bui-minor-secondary-action-button' : !this.props.primary,
-            });
+            };
         }
-        
+
+        const buttonClass = classNames(primaryOrSecondaryClass, {
+            'bui-is-selected': this.props.selected,
+            'bui-is-flat': this.props.flat,
+        });
+
+        const props = this.props;
         return (
-            <div className={buttonClass} onClick={this.props.onClick}>
+            <div {...props} className={buttonClass}>
                 {this.props.label}
             </div>
         );
