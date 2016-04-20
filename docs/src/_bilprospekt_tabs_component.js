d7870bef4aee5ff5e3a77b76676cfda8e50ca42f
Dan Andersson <dan@bilprospekt.se>
2016-04-20 14:24:06 +0200

    Added possibiliy to change tab content with the selectedTab prop instead of just adjusting the intial tab.

--- docs/src/_bilprospekt_tabs_component.js
+++ docs/src/_bilprospekt_tabs_component.js
@@ -13,6 +13,10 @@ const TabsDoc = React.createClass({
         this.setState({ index });
     },
 
+    _updateSelectedTab(index) {
+        this.setState({ index: index });
+    },
+
     render() {
         const tabLabels = [
             'Allmänt',
@@ -24,7 +28,13 @@ const TabsDoc = React.createClass({
         return (
             <div id='TabsDoc'>
                 <p className="table-header-label">Tabs</p>
-                <Tabs labels={tabLabels} selectedTab={0} onChange={this._onChange}>
+
+                <p onClick={this._updateSelectedTab.bind(this, 0)}>Allmänt</p>
+                <p onClick={this._updateSelectedTab.bind(this, 1)}>Konto</p>
+                <p onClick={this._updateSelectedTab.bind(this, 2)}>Förval</p>
+                <p onClick={this._updateSelectedTab.bind(this, 3)}>Tid och Datum</p>
+
+                <Tabs labels={tabLabels} selectedTab={this.state.index} onChange={this._onChange}>
                     <p>Tab {this.state.index} Content</p>
                 </ Tabs>
 
