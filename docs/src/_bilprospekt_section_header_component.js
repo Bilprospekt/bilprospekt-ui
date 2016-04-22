e96f37c5726299b98c4920ec65e2b528f053f488
Dan Andersson <dan@bilprospekt.se>
2016-04-22 17:02:57 +0200

    Added some new minor props.

--- docs/src/_bilprospekt_section_header_component.js
+++ docs/src/_bilprospekt_section_header_component.js
@@ -9,6 +9,9 @@ const SectionHeaderDoc = React.createClass({
                 <p className="table-header-label">Section Header</p>
                 <SectionHeader highlighted icon='fa-user' label='User Profile' desc='This user is highlighted' style={{marginBottom: 20}} />
                 <SectionHeader icon='fa-comment' label='Comment Section' desc='This is where we comment on stuff' style={{marginBottom: 20}} />
+                <SectionHeader icon='fa-users' label='Party Section' desc={['Peter', 'Daniel', 'Alexander']} style={{marginBottom: 20}} />
+                <SectionHeader icon='fa-car' label='Car Section' desc='Something minor happened!' descType='minor' style={{marginBottom: 20}} />
+                <SectionHeader icon='fa-building' label='Company Section' desc='Something major happened!' descType='major' style={{marginBottom: 20}} />
 
                 <pre>
                 <code>
@@ -17,7 +20,8 @@ const SectionHeaderDoc = React.createClass({
                         '<SectionHeader',
                         '\n\t icon="string"',
                         '\n\t label="string"',
-                        '\n\t desc="string"',
+                        '\n\t desc="string" || desc=[array]',
+                        '\n\t descType="minor" || descType="major"',
                         '\n\t style={object}',
                         '\n\t highlighted={bool}',
                         '\n/>'
--- src/section-header/index.js
+++ src/section-header/index.js
@@ -1,18 +1,50 @@
 import React 	  from 'react';
+import _		  from 'underscore';
 import classNames from 'classnames';
 
 const SectionHeader = React.createClass({
 	propTypes: {
-        icon:  		 React.PropTypes.string.isRequired,
-        label: 		 React.PropTypes.string.isRequired,
-        desc:  		 React.PropTypes.string,
+        icon: React.PropTypes.string.isRequired,
+        label: React.PropTypes.string.isRequired,
+        desc: React.PropTypes.oneOfType([
+        	React.PropTypes.string,
+        	React.PropTypes.array,
+        ]),
+        descType: React.PropTypes.string,
         highlighted: React.PropTypes.bool,
-        style: 		 React.PropTypes.func,
+        style: React.PropTypes.func,
     },
+
+    getDefaultProps() {
+    	return {
+    		descType: 'normal',
+    	};
+    },
+
 	render() {
 		const parentClass = classNames('bui-section-header', {
 			'is-highlighted': this.props.highlighted,
 		});
+
+		// this.props.descType
+		let descIcon = (this.props.descType === 'minor' || this.props.descType === 'major')
+			? <i className='desc-icon fa fa-warning' />
+			: null ;
+
+		const descClass = classNames('section-desc', {
+			'is-minor': this.props.descType === 'minor',
+			'is-major': this.props.descType === 'major',
+		});
+
+		// this.props.desc
+		let descText;
+		if (_.isArray(this.props.desc)) {
+			descText = _.map(this.props.desc, (string, i) => {
+				return (i === this.props.desc.length - 1) ? string : string + ', ' ;
+			});
+		} else {
+			descText = this.props.desc;
+		}
 
 		return (
 			<div className={parentClass} style={this.props.style}>
@@ -21,7 +53,7 @@ const SectionHeader = React.createClass({
 				</div>
 				<div className='section-text'>
 					<p className='section-label'>{this.props.label}</p>
-					<p className='section-desc'>{this.props.desc}</p>
+					<p className={descClass}>{descIcon}{descText}</p>
 				</div>
 			</div>
 		);
