51ab99ce6f7410026e38828bb1a5621752e025d7
Isak Larsson <isak@Isaks-MacBook-Pro.local>
2018-11-07 11:09:07 +0100

    updated table_header with a proper toggler for active columns in dropdown

--- src/table/table_header.js
+++ src/table/table_header.js
@@ -132,135 +132,140 @@ const TableHeader = React.createClass({
 
         const props = this.props;
         let columnChanger = null;
-        if (props.allColumnsThatCouldBeRendered && props.allColumnsThatCouldBeRendered.length && props.currentColumns) {
-            const columns = _(props.allColumnsThatCouldBeRendered).map((column, key) => {
-                const checked = (typeof column.active !== undefined) ? column.active : _(props.currentColumns).findWhere({val : column.val});
-                return <DropdownElement key={key} checkboxChecked={!!checked} checkbox label={column.label} onClick={this._onColumnChange.bind(this, column.val)} />
-            });
 
-            columnChanger = (
-                <div>
-                    <DropdownHolder ref='dropdownHolder' noArrow orientation="right" icon="fa-ellipsis-h table-icon">
-                        {columns}
-                    </DropdownHolder>
-                </div>
-            );
+        //Array of columns which should be active
+      let tempCurrentColumnArr = (typeof props.currentColumns[0] === 'string') ? props.currentColumns : props.currentColumns.map(x => x.val);
 
-            columnChanger = <Tooltip string='Välj kolumner att visa' {...tooltipProps}>{columnChanger}</Tooltip>;
-        }
+      if (props.allColumnsThatCouldBeRendered && props.allColumnsThatCouldBeRendered.length && props.currentColumns) {
+          const columns = _(props.allColumnsThatCouldBeRendered).map((column, key) => {
+              //If column is in the currentColumn arr mark it as active.
+              const checked = column.active = tempCurrentColumnArr.indexOf(column.val) > -1;
+              return <DropdownElement key={key} checkboxChecked={checked} checkbox label={column.label} onClick={this._onColumnChange.bind(this, column.val)} />
+          });
 
-        const tableSearchClass = classNames('bui-table-search', {
-            'display-search': this.state.displaySearch,
-        });
+          columnChanger = (
+              <div>
+                  <DropdownHolder ref='dropdownHolder' noArrow orientation="right" icon="fa-ellipsis-h table-icon">
+                      {columns}
+                  </DropdownHolder>
+              </div>
+          );
 
-        let tableSearch = null;
-
-        if (this.props.useSearch) {
-            tableSearch = (
-                <div className={tableSearchClass}>
-                    <Tooltip string='Snabbsök i tabellen' {...tooltipProps}>
-                        <i className="fa fa-search table-icon" onClick={this._displaySearch} />
-                    </Tooltip>
-                    <InputField ref='tableSearchInputRef' icon="fa-search" hint={this.props.searchHint} fastRemove onChange={this.props.onSearchChange} onBlur={this._searchBlur} />
-                </div>
-            );
-        }
-
-        const filtersWithoutId = _(this.props.currentFilters).filter((n) => n[0] !== '_id');
-        //Filter Jawbone
-        const filterCount = (filtersWithoutId.length)
-                  ? <span className='toggle-filter-text'>{filtersWithoutId.length}</span>
-                  : null;
-
-        const jawboneParentClass = classNames('bui-table-toggle-filter-button', {
-            'bui-inactive-filter-button': !filtersWithoutId.length,
-        });
-
-        const jawboneFilter = (
-            <div className={jawboneParentClass}>
-                <i className="fa fa-filter table-icon" onClick={this.props.showJawboneFilter} />
-                {filterCount}
-            </div>
-        );
-
-
-        //Selections Icon
-        const selectionsParentClass = classNames('bui-table-toggle-filter-button', {
-            'bui-inactive-filter-button': !this.props.selections.length,
-        });
-
-        const selectionsCount = (this.props.selections.length)
-                  ? <span className='toggle-filter-text'>{this.props.selections.length}</span>
-                  : null;
-
-        let selectionsFilter = null;
-        if (this.props.makeRowsSelectable) {
-            selectionsFilter = (
-                <div className={selectionsParentClass}>
-                    <i className="fa fa-check table-icon" onClick={this._filterOnSelections} />
-                    {selectionsCount}
-                </div>
-            );
-            selectionsFilter = <Tooltip string='Visa markerade rader' {...tooltipProps}>{selectionsFilter}</Tooltip>;
-        }
-
-        // this.props.headerLabel
-        let headerLabel;
-        if (typeof this.props.headerLabel === 'string') {
-            if (this.props.selections.length) {
-                if (this.props.selections.length === 1) {
-                    headerLabel = <span className='table-active-selections-label'>Du har markerat {this.props.selections.length} rad</span>;
-                } else {
-                    headerLabel = <span className='table-active-selections-label'>Du har markerat {this.props.selections.length} rader</span>;
-                }
-            } else {
-                headerLabel = this.props.headerLabel;
-            }
-        } else if (_.isObject(this.props.headerLabel)) {
-            headerLabel = (
-                <SectionHeader
-                    icon={this.props.headerLabel.icon}
-                    label={this.props.headerLabel.label}
-                    desc={this.props.headerLabel.desc}
-                    descType={this.props.headerLabel.descType}
-                    actionLabel={this.props.headerLabel.actionLabel}
-                    onAction={this.props.headerLabel.onAction}
-                    style={this.props.headerLabel.style}
-                    highlighted={this.props.headerLabel.highlighted}
-                    />
-            );
-        } else {
-            headerLabel = '';
-        }
-
-      // Override headerLabel with actionHeaderButtons
-      if (this.props.actionHeaderButtons && this.props.actionHeaderButtons.length) {
-        let i = 0;
-        let buttons = [];
-        for (let i = 0; i < this.props.actionHeaderButtons.length; i += 1) {
-          let v = this.props.actionHeaderButtons[i];
-          let b = <p key={i} onClick={v.onClick}><i className={'fa fa-' + v.iconClass} />{v.label}</p>;
-          buttons.push(b);
-        }
-        headerLabel = <div className='bui-table-action-header-buttons'>{buttons}</div>;
+          columnChanger = <Tooltip string='Välj kolumner att visa' {...tooltipProps}>{columnChanger}</Tooltip>;
       }
-        
 
-        return (
-            <div className='bui-table-action-bar'>
-                <div className='bui-table-text-holder'>
-                    {headerLabel}
-                </div>
-                <div className='bui-table-actions-holder'>
-                    {tableSearch}
-                    <Tooltip string='Visa valda filter' {...tooltipProps}>{jawboneFilter}</Tooltip>
-                    <Tooltip string='Återställ kolumnbredder' {...tooltipProps}><i className="fa fa-arrows-h table-icon" onClick={this.props.justifyColumns} /></Tooltip>
-                    {selectionsFilter}
-                    {columnChanger}
-                </div>
-            </div>
-        )
+      const tableSearchClass = classNames('bui-table-search', {
+          'display-search': this.state.displaySearch,
+      });
+
+      let tableSearch = null;
+
+      if (this.props.useSearch) {
+          tableSearch = (
+              <div className={tableSearchClass}>
+                  <Tooltip string='Snabbsök i tabellen' {...tooltipProps}>
+                      <i className="fa fa-search table-icon" onClick={this._displaySearch} />
+                  </Tooltip>
+                  <InputField ref='tableSearchInputRef' icon="fa-search" hint={this.props.searchHint} fastRemove onChange={this.props.onSearchChange} onBlur={this._searchBlur} />
+              </div>
+          );
+      }
+
+      const filtersWithoutId = _(this.props.currentFilters).filter((n) => n[0] !== '_id');
+      //Filter Jawbone
+      const filterCount = (filtersWithoutId.length)
+                ? <span className='toggle-filter-text'>{filtersWithoutId.length}</span>
+                : null;
+
+      const jawboneParentClass = classNames('bui-table-toggle-filter-button', {
+          'bui-inactive-filter-button': !filtersWithoutId.length,
+      });
+
+      const jawboneFilter = (
+          <div className={jawboneParentClass}>
+              <i className="fa fa-filter table-icon" onClick={this.props.showJawboneFilter} />
+              {filterCount}
+          </div>
+      );
+
+
+      //Selections Icon
+      const selectionsParentClass = classNames('bui-table-toggle-filter-button', {
+          'bui-inactive-filter-button': !this.props.selections.length,
+      });
+
+      const selectionsCount = (this.props.selections.length)
+                ? <span className='toggle-filter-text'>{this.props.selections.length}</span>
+                : null;
+
+      let selectionsFilter = null;
+      if (this.props.makeRowsSelectable) {
+          selectionsFilter = (
+              <div className={selectionsParentClass}>
+                  <i className="fa fa-check table-icon" onClick={this._filterOnSelections} />
+                  {selectionsCount}
+              </div>
+          );
+          selectionsFilter = <Tooltip string='Visa markerade rader' {...tooltipProps}>{selectionsFilter}</Tooltip>;
+      }
+
+      // this.props.headerLabel
+      let headerLabel;
+      if (typeof this.props.headerLabel === 'string') {
+          if (this.props.selections.length) {
+              if (this.props.selections.length === 1) {
+                  headerLabel = <span className='table-active-selections-label'>Du har markerat {this.props.selections.length} rad</span>;
+              } else {
+                  headerLabel = <span className='table-active-selections-label'>Du har markerat {this.props.selections.length} rader</span>;
+              }
+          } else {
+              headerLabel = this.props.headerLabel;
+          }
+      } else if (_.isObject(this.props.headerLabel)) {
+          headerLabel = (
+              <SectionHeader
+                  icon={this.props.headerLabel.icon}
+                  label={this.props.headerLabel.label}
+                  desc={this.props.headerLabel.desc}
+                  descType={this.props.headerLabel.descType}
+                  actionLabel={this.props.headerLabel.actionLabel}
+                  onAction={this.props.headerLabel.onAction}
+                  style={this.props.headerLabel.style}
+                  highlighted={this.props.headerLabel.highlighted}
+                  />
+          );
+      } else {
+          headerLabel = '';
+      }
+
+    // Override headerLabel with actionHeaderButtons
+    if (this.props.actionHeaderButtons && this.props.actionHeaderButtons.length) {
+      let i = 0;
+      let buttons = [];
+      for (let i = 0; i < this.props.actionHeaderButtons.length; i += 1) {
+        let v = this.props.actionHeaderButtons[i];
+        let b = <p key={i} onClick={v.onClick}><i className={'fa fa-' + v.iconClass} />{v.label}</p>;
+        buttons.push(b);
+      }
+      headerLabel = <div className='bui-table-action-header-buttons'>{buttons}</div>;
     }
+
+
+      return (
+          <div className='bui-table-action-bar'>
+              <div className='bui-table-text-holder'>
+                  {headerLabel}
+              </div>
+              <div className='bui-table-actions-holder'>
+                  {tableSearch}
+                  <Tooltip string='Visa valda filter' {...tooltipProps}>{jawboneFilter}</Tooltip>
+                  <Tooltip string='Återställ kolumnbredder' {...tooltipProps}><i className="fa fa-arrows-h table-icon" onClick={this.props.justifyColumns} /></Tooltip>
+                  {selectionsFilter}
+                  {columnChanger}
+              </div>
+          </div>
+      )
+  }
 
 });
 
